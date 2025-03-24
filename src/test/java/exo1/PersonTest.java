45703451c2c016ce8444b6b8031e357e25509b5d
anwer <anwer7149@gmail.com>
2025-03-24 10:32:59 +0100

    test methode isAdult case 2 : minor

--- src/test/java/exo1/PersonTest.java
+++ src/test/java/exo1/PersonTest.java
@@ -10,12 +10,18 @@ class PersonTest {
         assertEquals("John Doe", person.getFullName());
     }
 
-
     @org.junit.jupiter.api.Test
     public void testIsAdultWhenAdult() {
         Person person = new Person("John", "Doe", 25);
         assertTrue(person.isAdult());
     }
+
+    @org.junit.jupiter.api.Test
+    public void testIsAdultWhenMinor() {
+        Person person = new Person("Jane", "Doe", 16);
+        assertFalse(person.isAdult());
+    }
+
 
 
 }
