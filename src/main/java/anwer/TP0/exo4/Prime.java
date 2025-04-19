8518b20404dc8c4120ccd818d0f52d4867fa48e9
anwer <anwer7149@gmail.com>
2025-04-19 18:49:05 +0100

    l'ajout de classe Prime

---
+++ src/main/java/anwer/TP0/exo4/Prime.java
@@ -0,0 +1,24 @@
+package anwer.TP0.exo4;
+
+public class Prime {
+
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        int sqrtN = (int) Math.sqrt(n) + 1;
+        for (int i = 3; i <= sqrtN; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
