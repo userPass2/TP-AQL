7699fd3b68ebc71ce28738cece922ef5f52b7783
anwer <anwer7149@gmail.com>
2025-04-19 18:46:28 +0100

    test de class Personne

---
+++ src/test/java/anwer/TP0/exo1/PersonTest.java
@@ -0,0 +1,36 @@
+package TP0.exo1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import TP0.exo1.Person;
+import  org.junit.jupiter.api.Test ;
+class PersonTest {
+
+    @Test
+    public void testGetFullName() {
+        Person person = new Person("azrty", "dell", 25);
+        assertEquals("azrty dell", person.getFullName());
+    }
+
+    @Test
+    public void testIsAdultWhenAdult() {
+        Person person = new Person("azrty", "dell", 25);
+        assertTrue(person.isAdult());
+    }
+
+    @Test
+    public void testIsAdultWhenMinor() {
+        Person person = new Person("azrty", "dell", 16);
+        assertFalse(person.isAdult());
+    }
+
+    @Test
+    public void testIsAdultWhenExactly18() {
+        Person person = new Person("azrty", "dell", 18);
+        assertTrue(person.isAdult());
+    }
+
+
+
+
+}
